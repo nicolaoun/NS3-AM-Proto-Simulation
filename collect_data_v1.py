8b5ade3483d82774a84c80933a79a095d50bdb72
apache <apache@drivenest.com>
2016-08-16 10:37:59 +0200

    cleaned a little collect_data code

--- collect_data_v1.py
+++ collect_data_v1.py
@@ -22,6 +22,8 @@ def get_single_test_avg_results(filename,numR):
 	w_avgCommtime = 0
 	w_avgComptime = 0
 
+	numR_completed = 0
+
 
 	with open(filename) as f:
 		file_lines = []
@@ -40,8 +42,13 @@ def get_single_test_avg_results(filename,numR):
 				if("** READER_" in line):
 					r_sentMsgs += int(line.split("Msgs=")[1].split(",")[0])
 					r_invokedReads += int(line.split("InvokedReads=")[1].split(",")[0])
-					r_completedReads += int(line.split("CompletedReads=")[1].split(",")[0])
-                                        if(protocol==1) or (protocol==6) or (protocol==11) or (protocol==16) or (protocol==21): #ABD
+					
+					completed = int(line.split("CompletedReads=")[1].split(",")[0])
+					if completed > 0:	
+						numR_completed += 1
+					r_completedReads += completed 
+        
+					if(protocol==1) or (protocol==6) or (protocol==11) or (protocol==16) or (protocol==21): #ABD
 						r_slowReads += int(line.split("4EXCH_reads=")[1].split(",")[0])
 						r_fastReads += 0
                                         elif (protocol==2) or (protocol==7) or (protocol==12) or (protocol==17) or (protocol==22): #ohSAM
@@ -62,12 +69,12 @@ def get_single_test_avg_results(filename,numR):
 					aveComm = line.split("AveCommTime=")[1].split("s")[0]
 					if( aveComm != "nan" and aveComm != "-inf" and aveComm != "inf"):  
 						r_avgCommtime += float(aveComm)		#ok
-						##print "** AverageCommunicationTime = " + aveComm
+						## print "** AverageCommunicationTime = " + aveComm
 					
 					aveComp = line.split("AvgCompTime=")[1].split("s")[0]
 					if( aveComp != "nan" and aveComp != "-inf" and aveComp != "inf"):  
 						r_avgComptime += float(aveComp)		#ok
-						print "** AverageComputationTime = " + aveComp
+						## print "** AverageComputationTime = " + aveComp
 					#r_avgComptime += float(line.split("AvgCompTime=")[1].split("s")[0])		#ok
 	
 	# To be sure not to divide by 0 at any time and break everything
@@ -77,10 +84,19 @@ def get_single_test_avg_results(filename,numR):
 		ratio=r_fastReads/(r_slowReads+r_fastReads)
 		percent=(r_slowReads/(r_fastReads+r_slowReads))*100
 	
+	# compute avg times
+	if (numR_completed == 0): 
+		r_avgtime = 0
+		r_avgCommtime = 0
+		r_avgComptime = 0
+	else:
+		r_avgtime = r_avgtime/numR_completed
+		r_avgCommtime = r_avgCommtime/numR_completed
+		r_avgComptime = r_avgComptime/numR_completed
 	# we have to return this: "fastOps slowOps readRatio readPrcnt readAvgT rdrMsgs wtMsgs srvMsgs\n"
 	#return r_fastReads , r_slowReads , ratio , percent , r_avgtime/numR , r_sentMsgs , w_sentMsgs, srv_msgs
 	# we have to return this: "fastOps slowOps readRatio readPrcnt readAvgT readAvgCommT readAvgCompT rdrMsgs wtMsgs srvMsgs\n"
-	return r_fastReads , r_slowReads , ratio , percent , r_avgtime/numR , r_avgCommtime/numR , r_avgComptime/numR , r_sentMsgs , w_sentMsgs, srv_msgs
+	return r_fastReads , r_slowReads , ratio , percent , r_avgtime , r_avgCommtime , r_avgComptime , r_sentMsgs , w_sentMsgs, srv_msgs
 
 
 
@@ -111,6 +127,8 @@ def execute():
 	main_avg_rdrMsgs=0
 	main_avg_wrtMsgs=0
 	main_avg_srvMsgs=0
+
+	success_tests=0
 	
 	seed=0
 
@@ -157,21 +175,24 @@ def execute():
 		main_avg_wrtMsgs += avgs_list[8]
 		main_avg_srvMsgs += avgs_list[9]
 		seed += 19
+
+		if avgs_list[4] > 0:
+			success_tests += 1
 	
 	# Once all the tests finish now we have to AVG_ALL the test and write them to the main file!!!
 	# Process them here!
-	main_avg_fastOps = main_avg_fastOps / tests
-	main_avg_slowOps = main_avg_slowOps / tests
-	main_avg_readRatio = main_avg_readRatio / tests
-	main_avg_readPrcnt = main_avg_readPrcnt / tests
-	main_avg_readTime = main_avg_readTime / tests
-	main_avg_readCommTime = main_avg_readCommTime / tests
-	main_avg_readCompTime = main_avg_readCompTime / tests
-	main_avg_Total_rdrMsgs = main_avg_rdrMsgs / tests
-	main_avg_rdrMsgs = main_avg_rdrMsgs / tests / numReaders
-	main_avg_wrtMsgs = main_avg_wrtMsgs / tests
-	main_avg_Total_srvMsgs = main_avg_srvMsgs / tests
-	main_avg_srvMsgs = main_avg_srvMsgs / tests / numServers
+	main_avg_fastOps = main_avg_fastOps /success_tests
+	main_avg_slowOps = main_avg_slowOps / success_tests
+	main_avg_readRatio = main_avg_readRatio / success_tests
+	main_avg_readPrcnt = main_avg_readPrcnt / success_tests
+	main_avg_readTime = main_avg_readTime / success_tests
+	main_avg_readCommTime = main_avg_readCommTime / success_tests
+	main_avg_readCompTime = main_avg_readCompTime / success_tests
+	main_avg_Total_rdrMsgs = main_avg_rdrMsgs / success_tests
+	main_avg_rdrMsgs = main_avg_rdrMsgs / success_tests / numReaders
+	main_avg_wrtMsgs = main_avg_wrtMsgs / success_tests
+	main_avg_Total_srvMsgs = main_avg_srvMsgs / success_tests
+	main_avg_srvMsgs = main_avg_srvMsgs / success_tests / numServers
 	
 
 
@@ -212,9 +233,11 @@ rds_step=10  #No worries we will end up doing 10,20,40,80,100
 vrsn_start=0
 vrsn_stop=1
 vrsn_step=1
-prtcl_start = 16
-prtcl_stop = 25
-prtcl_step=1
+#prtcl_start = 16
+#prtcl_stop = 25
+#prtcl_step=1
+protocols = [ ["abd", "am-abd"], ["oh-Sam", "am-ohSam"], ["Semifast", "am-semifast"], ["HybridFast", "am-cchybrid"], ["oh-Fast", "am-ohfast"] ]
+topologies = [ "spike", "star", "p2p", "star-p2p" ]
 srvrs_start=10 #10
 srvrs_stop=30
 srvrs_step=5
@@ -254,169 +277,108 @@ print "Starting Script...\n"
 
 if len(sys.argv) == 2:
 	if (int(sys.argv[1]) == 1):
-		print "Algorithm ABD\n"
-		prtcl_start=1
-		prtcl_stop=1
+            print "Algorithm ABD\n"
+            prtcl_start=1
+            prtcl_stop=1
 	elif(int(sys.argv[1]) == 2):
-		print "Algorithm oh-Sam\n"
-		prtcl_start=2
-		prtcl_stop=2
+            print "Algorithm oh-Sam\n"
+            prtcl_start=2
+            prtcl_stop=2
 	elif(int(sys.argv[1]) == 3):
-		print "Algorithm SemiFast\n"
-		prtcl_start=3
-		prtcl_stop=3
+            print "Algorithm SemiFast\n"
+            prtcl_start=3
+            prtcl_stop=3
 	elif(int(sys.argv[1]) == 4):
-		print "Algorithm ccHybrid\n"
-		prtcl_start=4
-		prtcl_stop=4
+            print "Algorithm ccHybrid\n"
+            prtcl_start=4
+            prtcl_stop=4
 	elif(int(sys.argv[1]) == 5):
-		print "Algorithm oh-Fast\n"
-		prtcl_start=5
-		prtcl_stop=5
+            print "Algorithm oh-Fast\n"
+            prtcl_start=5
+            prtcl_stop=5
 	elif(int(sys.argv[1]) == 6):
-		print "Algorithm ABD SPIKE\n"
-		prtcl_start=6
-		prtcl_stop=6
+            print "Algorithm ABD SPIKE\n"
+            prtcl_start=6
+            prtcl_stop=6
 	elif(int(sys.argv[1]) == 7):
-		print "Algorithm oh-Sam SPIKE\n"
-		prtcl_start=7
-		prtcl_stop=7
+            print "Algorithm oh-Sam SPIKE\n"
+            prtcl_start=7
+            prtcl_stop=7
 	elif(int(sys.argv[1]) == 8):
-		print "Algorithm oh-Fast SPIKE\n"
-		prtcl_start=8
-		prtcl_stop=8
+            print "Algorithm oh-Fast SPIKE\n"
+            prtcl_start=8
+            prtcl_stop=8
 	elif(int(sys.argv[1]) == 9):
-		print "Algorithm ABD STAR\n"
-		prtcl_start=9
-		prtcl_stop=9
+            print "Algorithm ABD STAR\n"
+            prtcl_start=9
+            prtcl_stop=9
 	elif(int(sys.argv[1]) == 10):
-		print "Algorithm oh-Sam STAR\n"
-		prtcl_start=10
-		prtcl_stop=10
+            print "Algorithm oh-Sam STAR\n"
+            prtcl_start=10
+            prtcl_stop=10
 	elif(int(sys.argv[1]) == 11):
-		print "Algorithm oh-Fast STAR\n"
-		prtcl_start=11
-		prtcl_stop=11
+            print "Algorithm oh-Fast STAR\n"
+            prtcl_start=11
+            prtcl_stop=11
+        elif(int(sys.argv[1]) == 30):
+            print "Version fixInt"
+            vrsn_start=0
+            vrsn_stop=0
+        elif(int(sys.argv[1]) == 31):
+            print "Version randInt"
+            vrsn_start=1
+            vrsn_stop=1
 	elif(int(sys.argv[1]) == 50):
-		executeFlag=0
+            executeFlag=0
 	else:
-		print "Wrong Algorithm Selection\n"
-		exit(0)
+            print "Not Valid Argument\n"
+            exit(0)
 
 
 
 #for all the fail scenario (1,3)
 for fail in range(fail_start,fail_stop+1,fail_step):
+    for version in range(vrsn_start, vrsn_stop+1, vrsn_step):
+        if(version==0):
+            Version="fixInt"
+        else:
+            Version = "randInt"
+
+        print "For Version="+str(Version)+":"
+
 	#print fail
 	#range(start, stop, step) (last step is not included
-	main_results_dir = "output/ALL_RESULTS_fail_"+str(fail)+".txt"
-        print "*** For Failures="+str(fail)+" the Main Averaged Results will be at: " + str(main_results_dir)+" ***"
+        main_results_dir = "output/ALL_RESULTS_fail_"+str(fail)+"_version_"+str(Version)+".txt"
+        print "*** The Main Averaged Results will be at: " + str(main_results_dir)+" ***"
 	main_directory = create_output_file_for_scenario(main_results_dir,0)
-	for numServers in range(srvrs_start, srvrs_stop+1, srvrs_step):
+
+        for numServers in range(srvrs_start, srvrs_stop+1, srvrs_step):
                 print "--Scenario for Servers="+str(numServers)+"--"
 		# for each protocol (up to 6)
-		for protocol in range(prtcl_start,prtcl_stop+1,prtcl_step):
-			# for each number of readers (up to 10,20,40,80)
-			if (protocol==1):
-				alg=abd
-				executable="am-abd"
-			elif (protocol==2):
-				alg=oh_sam
-				executable="am-ohSam"
-			elif (protocol==3):
-				alg=semifast
-				executable="am-semifast"
-			elif (protocol==4):
-				alg=hybridfast
-				executable="am-cchybrid"
-			elif (protocol==5):
-				alg=oh_fast
-				executable="am-ohfast"
-			elif (protocol==6):
-                                alg=abd+"-spike"
-				executable="am-abd-spike"
-			elif (protocol==7):
-                                alg=oh_sam+"-spike"
-				executable="am-ohSam-spike"
-			elif (protocol==8):
-                                alg=semifast+"-spike"
-				executable="am-semifast-spike"
-			elif (protocol==9):
-                                alg=hybridfast+"-spike"
-				executable="am-cchybrid-spike"
-			elif (protocol==10):
-                                alg=oh_fast+"-spike"
-				executable="am-ohfast-spike"
-			elif (protocol==11):
-                                alg=abd+"-star"
-				executable="am-abd-star"
-			elif (protocol==12):
-                                alg=oh_sam+"-star"
-				executable="am-ohSam-star"
-			elif (protocol==13):
-                                alg=semifast+"-star"
-				executable="am-semifast-star"
-			elif (protocol==14):
-                                alg=hybridfast+"-star"
-				executable="am-cchybrid-star"
-			elif (protocol==15):
-                                alg=oh_fast+"-star"
-				executable="am-ohfast-star"
-                        elif (protocol==16):
-                                alg=abd+"-p2p"
-                                executable="am-abd-p2p"
-                        elif (protocol==17):
-                                alg=oh_sam+"-p2p"
-                                executable="am-ohSam-p2p"
-                        elif (protocol==18):
-                                alg=semifast+"-p2p"
-                                executable="am-semifast-p2p"
-                        elif (protocol==19):
-                                alg=hybridfast+"-p2p"
-                                executable="am-cchybrid-p2p"
-                        elif (protocol==20):
-                                alg=oh_fast+"-p2p"
-                                executable="am-ohfast-p2p"
-                        elif (protocol==21):
-                                alg=abd+"-star-p2p"
-                                executable="am-abd-star-p2p"
-                        elif (protocol==22):
-                                alg=oh_sam+"-star-p2p"
-                                executable="am-ohSam-star-p2p"
-                        elif (protocol==23):
-                                alg=semifast+"-star-p2p"
-                                executable="am-semifast-star-p2p"
-                        elif (protocol==24):
-                                alg=hybridfast+"-star-p2p"
-                                executable="am-cchybrid-star-p2p"
-                        elif (protocol==25):
-                                alg=oh_fast+"-star-p2p"
-                                executable="am-ohfast-star-p2p"
+                #for protocol in range(prtcl_start,prtcl_stop+1,prtcl_step):
+                for topology in topologies:
+                    for alg, executable in protocols:
+                        # for each number of readers (up to 10,20,40,80)
+                        executable = executable +"-"+ topology
 
-			print "  For Algorithm="+str(executable)+":"
-			#s - 15 r -40
+                        print "  For Algorithm="+str(executable)+":"
+                        #s - 15 r -40
 
-			for numReaders in range(rdrs_start,rdrs_stop+1, rds_step):
-				if (numReaders==10) or (numReaders==20) or (numReaders==40) or (numReaders==80) or (numReaders==100):
-                                        print "    For Readers="+str(numReaders)+":"
-					for version in range(vrsn_start, vrsn_stop+1, vrsn_step):
-						if(version==0):
-							Version="fixInt"
-						else:
-							Version = "randInt"
-						# bar=bar+1
-						# sys.stdout.write('\r')
-						# sys.stdout.write("[%-100s] %d%%" % ('='*bar, bar))
-						# sys.stdout.flush()
-						
-                                                print "     For Version="+str(Version)+":"
-						for readInterval in range(rInterval_start, rInterval_stop+1, rInterval_step):
-							rInterval = float(readInterval)/10
-                                                        print "      For readInterval="+str(rInterval)+":"
-							for writeInterval in range(wInterval_start, wInterval_stop+1, wInterval_step):
-								wInterval = float(writeInterval)/10
-                                                                print "       For writeInterval="+str(wInterval)+":"
-								execute()
+                        for numReaders in range(rdrs_start,rdrs_stop+1, rds_step):
+                            if (numReaders==10) or (numReaders==20) or (numReaders==40) or (numReaders==80) or (numReaders==100):
+                                print "    For Readers="+str(numReaders)+":"
+                                # bar=bar+1
+                                # sys.stdout.write('\r')
+                                # sys.stdout.write("[%-100s] %d%%" % ('='*bar, bar))
+                                # sys.stdout.flush()
+						                                            
+                            for readInterval in range(rInterval_start, rInterval_stop+1, rInterval_step):
+                                rInterval = float(readInterval)/10
+                                print "      For readInterval="+str(rInterval)+":"
+                                for writeInterval in range(wInterval_start, wInterval_stop+1, wInterval_step):
+                                    wInterval = float(writeInterval)/10
+                                    print "       For writeInterval="+str(wInterval)+":"
+                                    execute()
 
 
 print "\n\nAll the work is done, Script Exiting..."
